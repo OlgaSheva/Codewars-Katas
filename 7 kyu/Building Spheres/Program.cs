2788edac77e3aeb88cfe763f1a31024e26f7895f
Olga Kripulevich <ochuvasheva@gmail.com>
2019-02-23 00:27:24 +0300

    7 kyu

---
+++ 7 kyu/Building Spheres/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Building_Spheres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sphere ball = new Sphere(2, 50);
+            Console.WriteLine(ball.GetVolume());
+            Console.WriteLine(ball.GetSurfaceArea());
+            Console.ReadKey();
+        }
+    }
+
+    public class Sphere
+    {
+        int radius;
+        int mass;
+
+        public Sphere(int radius, int mass)
+        {
+            this.radius = radius;
+            this.mass = mass;
+        }
+
+        public int GetRadius() { return radius; }
+        public int GetMass() { return mass; }
+        public double GetVolume() { return Math.Round(4 * Math.PI * Math.Pow(radius, 3) / 3, 5); }
+        public double GetSurfaceArea() { return Math.Round(4 * Math.PI * radius* radius, 5); }
+        public double GetDensity() { return Math.Round(mass / (4 * Math.PI * Math.Pow(radius, 3) / 3), 5); }
+    }
+}
