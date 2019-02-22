a3deb68bde62d738d5fe5d96d4106f82c57493d6
Olga Kripulevich <ochuvasheva@gmail.com>
2019-02-22 15:03:59 +0300

    OOP

---
+++ Basic subclasses - Adam and Eve/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Basic_subclasses___Adam_and_Eve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class God
+    {
+        public static Human[] Create()
+        {
+            Man man = new Man();
+            Woman woman = new Woman();
+
+            Human[] human = { man, woman };
+            return human;
+        }        
+    }
+
+    public class Human { }
+
+    public class Man : Human { }
+
+    public class Woman : Human { }
+}
---
+++ Fun with ES6 Classes 1 - People, people, people/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fun_with_ES6_Classes_1___People__people__people
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Person(string firstName = "John", string lastName = "Doe", int age = 0, string gender = "Male")
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string SayFullName()
+        {
+            string fullname = FirstName + " " + LastName;
+            return fullname;
+        }
+
+        public static string GreetExtraTerrestrials(string raceName)
+        {
+            return "Welcome to Planet Earth " + raceName;
+        }
+    }
+}
