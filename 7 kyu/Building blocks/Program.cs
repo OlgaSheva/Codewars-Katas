76af8dfd1b34217ee935bfad5efdff3b70e33bc7
Olga Kripulevich <ochuvasheva@gmail.com>
2019-02-22 23:49:45 +0300

    7 kyu

--- 7 kyu/Building blocks/Program.cs
+++ 7 kyu/Building blocks/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Building_blocks
 {
@@ -10,6 +6,28 @@ namespace Building_blocks
     {
         static void Main(string[] args)
         {
+            Block b = new Block(new int[] { 2, 2, 2 });
+            Console.WriteLine(b.GetSurfaceArea());
+            Console.ReadKey();
         }
+    }
+
+    class Block
+    {
+        static int Width { get; set; }
+        static int Length { get; set; }
+        static int Height { get; set; }
+
+        public Block(int[] arguments) {
+            Width = arguments[0];
+            Length = arguments[1];
+            Height = arguments[2];
+        }
+
+        public int GetWidth() { return Width; }
+        public int GetLength() { return Length; }
+        public int GetHeight() { return Height; }
+        public int GetVolume() { return Width * Length * Height; }
+        public int GetSurfaceArea() { return 2*(Width*Length + Length*Height + Height*Width); }
     }
 }
