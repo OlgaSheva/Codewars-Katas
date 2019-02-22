9d3a2a3a7a11ff3ce1f24345e6259537a58cc830
Olga Kripulevich <ochuvasheva@gmail.com>
2019-02-22 15:43:22 +0300

    add kyu folders

---
+++ 7 kyu/Fun with ES6 Classes 2 - Animals and Inheritance/Fun with ES6 Classes 2 - Animals and Inheritance/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fun_with_ES6_Classes_2___Animals_and_Inheritance
+{
+    public class Animal
+    {
+        public int Age;
+        public int Legs;
+        public string Name;
+        public string Species;
+        public string Status;
+
+        public Animal(string name, int age, int legs, string species, string status)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Legs = legs;
+            this.Species = species;
+            this.Status = status;
+        }
+
+        public virtual string Introduce()
+        {
+            return $"Hello, my name is {this.Name} and I am {this.Age} years old.";
+        }
+    }
+}
---
+++ 7 kyu/Fun with ES6 Classes 2 - Animals and Inheritance/Fun with ES6 Classes 2 - Animals and Inheritance/Class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Fun_with_ES6_Classes_2___Animals_and_Inheritance
+{
+    public class Shark : Animal
+    {
+        private static string species = "shark";
+        private static int legs = 0;
+        public Shark(string name, int age, string status) : base(name, age, legs, species, status) { }
+    }
+
+    public class Cat : Animal
+    {
+        private static string species = "cat";
+        private static int legs = 4;
+        public Cat(string name, int age, string status) : base(name, age, legs, species, status) { }
+
+        public override string Introduce()
+        {
+            return $"Hello, my name is {this.Name} and I am {this.Age} years old.  Meow meow!";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        private static string species = "dog";
+        private static int legs = 4;
+        private string Master;
+        public Dog(string name, int age, string status, string master) : base(name, age, legs, species, status)
+        {
+            this.Master = master;
+        }
+
+        public string GreetMaster()
+        {
+            return $"Hello {this.Master}";
+        }
+    }
+}
