b4d5a4ee0174d5f6c4cd3d1dcfe6929d83fb815b
Olga Kripulevich <ochuvasheva@gmail.com>
2019-02-22 20:41:51 +0300

    7 kyu

---
+++ 7 kyu/Building blocks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building_blocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 7 kyu/getNames()/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace getNames__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] data = new Person[]
+            {
+              new Person("Joe", 20),
+              new Person("Bill", 30),
+              new Person("Kate", 23)
+            };
+            
+            string[] names = Kata.GetNames(data);
+
+            for (int i = 0; i < data.Length; i++)
+                Console.WriteLine(data[i].Name);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Kata
+    {
+        public static string[] GetNames(Person[] data)
+        {
+            string[] names = new string[data.Length];
+            for (int i = 0; i < data.Length; i++)
+                names[i] = data[i].Name;
+            return names;
+        }
+    }
+
+    public class Person
+    {
+        public int Age;
+        public string Name;
+
+        public Person(string name = "John", int age = 21)
+        {
+            Age = age;
+            Name = name;
+        }
+    }
+
+}
