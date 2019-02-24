6b32ae595af0230f7d9e2cd71e4d20ee6080905a
Olga Kripulevich <ochuvasheva@gmail.com>
2019-02-25 00:39:07 +0300

    Adapter Pattern

--- 7 kyu/Adapter Pattern - Geese to Ducks/Adapter Pattern - Geese to Ducks/Class1.cs
+++ 7 kyu/Adapter Pattern - Geese to Ducks/Adapter Pattern - Geese to Ducks/Class1.cs
@@ -1,20 +1,39 @@
-﻿using System;
-
-public class GooseToIDuckAdapter
+﻿namespace AdapterPattern
 {
-    public GooseToIDuckAdapter(Goose goose)
+    public class GooseToIDuckAdapter : IDuck
     {
+        Goose goose;
+        public GooseToIDuckAdapter(Goose goose)
+        {
+            this.goose = goose;
+        }
+
+        public void Fly()
+        {
+            goose.Fly();
+        }
+
+        public string Quack()
+        {
+            return goose.Honk();
+        }
     }
-}
 
-public interface IDuck
-{
-    string Quack();
-    void Fly();
-}
+    public interface IDuck
+    {
+        string Quack();
+        void Fly();
+    }
 
-public class Goose
-{
-    string Honk();
-    void Fly();
+    public class Goose
+    {
+        public string Honk()
+        {
+            return "Honk";
+        }
+        public void Fly()
+        {
+            
+        }
+    }
 }
