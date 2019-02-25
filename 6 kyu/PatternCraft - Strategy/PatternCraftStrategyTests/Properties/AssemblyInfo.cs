6f34067a8c6ed95a2f5ae8b0be3f287442e7262f
Olga Kripulevich <ochuvasheva@gmail.com>
2019-02-25 15:43:05 +0300

    PatternCraft-Strategy

---
+++ 6 kyu/PatternCraft - Strategy/PatternCraftStrategyTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PatternCraftStrategyTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("PatternCraftStrategyTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f2cf716f-c29f-4464-bde5-188b1b617efa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 6 kyu/PatternCraft - Strategy/PatternCraftStrategyTests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatternCraftStrategy;
+
+namespace PatternCraftStrategyTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void _0_WalkMove()
+        {
+            IUnit viking = new Viking();
+
+            viking.Move();
+            Assert.AreEqual(1, viking.Position);
+            viking.Move();
+            Assert.AreEqual(2, viking.Position);
+        }
+
+        [TestMethod]
+        public void _1_FlyMove()
+        {
+            IUnit viking = new Viking();
+            viking.MoveBehavior = new Fly();
+
+            viking.Move();
+            Assert.AreEqual(10, viking.Position);
+            viking.Move();
+            Assert.AreEqual(20, viking.Position);
+        }
+
+        [TestMethod]
+        public void _2_MixMove()
+        {
+            IUnit viking = new Viking();
+
+            viking.Move();
+            Assert.AreEqual(1, viking.Position);
+
+            viking.MoveBehavior = new Fly();
+            viking.Move();
+            Assert.AreEqual(11, viking.Position);
+        }
+    }
+}
