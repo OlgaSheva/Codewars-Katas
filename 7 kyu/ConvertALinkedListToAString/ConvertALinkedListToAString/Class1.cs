bd182cbb93138543d1e51305d5c6e7c7b598f11b
Olga Kripulevich <ochuvasheva@gmail.com>
2019-02-26 20:21:51 +0300

    ConvertALinkedListToAString

---
+++ 7 kyu/ConvertALinkedListToAString/ConvertALinkedListToAString/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace ConvertALinkedListToAString
+{
+    public class Node
+    {
+        Node head; // головной/первый элемент
+        Node tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+
+
+        public int Data { get; private set; }
+        public Node Next { get; private set; }
+    }
+
+    public class Kata
+    {
+        public static string Stringify(Node list)
+        {
+            string stringList = "";
+
+            while (list != null)
+            {
+                stringList += list.Data + " -> ";
+                list = list.Next;
+            }
+            stringList += "null";
+
+
+            return stringList;
+        }
+    }
+}
