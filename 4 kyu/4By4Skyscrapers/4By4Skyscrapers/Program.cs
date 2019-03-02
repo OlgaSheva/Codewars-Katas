fc4689ab78a0b77544826afb1a99edc07330a1eb
Olga Kripulevich <ochuvasheva@gmail.com>
2019-03-02 20:41:28 +0300

    RemoveRepeatedInRowsAndColumnsSkyscrapers

--- 4 kyu/4By4Skyscrapers/4By4Skyscrapers/Program.cs
+++ 4 kyu/4By4Skyscrapers/4By4Skyscrapers/Program.cs
@@ -37,6 +37,16 @@ namespace _4By4Skyscrapers
                     actual[i, j] = new List<int>() { 1, 2, 3, 4 };
 
             // build skyscrapers 4
+            BuildSkyscapers4(actual, clues);
+
+            RemoveRepeatedInRowsAndColumnsSkyscrapers(actual);
+
+
+            
+        }
+
+        public static void BuildSkyscapers4(List<int>[,] actual, int[] clues)
+        {
             foreach (int index in clues)
             {
                 if (index == 1)
@@ -45,98 +55,68 @@ namespace _4By4Skyscrapers
                     {
                         case 0:
                         case 15:
-                            actual[0, 0].Remove(1);
-                            actual[0, 0].Remove(2);
-                            actual[0, 0].Remove(3);
+                            actual[0, 0].RemoveRange(0, 2);
                             break;
                         case 1:
-                            actual[0, 1].Remove(1);
-                            actual[0, 1].Remove(2);
-                            actual[0, 1].Remove(3);
+                            actual[0, 1].RemoveRange(0, 2);
                             break;
                         case 2:
-                            actual[0, 2].Remove(1);
-                            actual[0, 2].Remove(2);
-                            actual[0, 2].Remove(3);
+                            actual[0, 2].RemoveRange(0, 2);
                             break;
                         case 3:
                         case 4:
-                            actual[0, 3].Remove(1);
-                            actual[0, 3].Remove(2);
-                            actual[0, 3].Remove(3);
+                            actual[0, 3].RemoveRange(0, 2);
                             break;
                         case 5:
-                            actual[1, 3].Remove(1);
-                            actual[1, 3].Remove(2);
-                            actual[1, 3].Remove(3);
+                            actual[1, 3].RemoveRange(0, 2);
                             break;
                         case 6:
-                            actual[2, 3].Remove(1);
-                            actual[2, 3].Remove(2);
-                            actual[2, 3].Remove(3);
+                            actual[2, 3].RemoveRange(0, 2);
                             break;
                         case 7:
                         case 8:
-                            actual[3, 3].Remove(1);
-                            actual[3, 3].Remove(2);
-                            actual[3, 3].Remove(3);
+                            actual[3, 3].RemoveRange(0, 2);
                             break;
                         case 9:
-                            actual[3, 2].Remove(1);
-                            actual[3, 2].Remove(2);
-                            actual[3, 2].Remove(3);
+                            actual[3, 2].RemoveRange(0, 2);
                             break;
                         case 10:
-                            actual[3, 1].Remove(1);
-                            actual[3, 1].Remove(2);
-                            actual[3, 1].Remove(3);
+                            actual[3, 1].RemoveRange(0, 2);
                             break;
                         case 11:
                         case 12:
-                            actual[3, 0].Remove(1);
-                            actual[3, 0].Remove(2);
-                            actual[3, 0].Remove(3);
+                            actual[3, 0].RemoveRange(0, 2);
                             break;
                         case 13:
-                            actual[2, 0].Remove(1);
-                            actual[2, 0].Remove(2);
-                            actual[2, 0].Remove(3);
+                            actual[2, 0].RemoveRange(0, 2);
                             break;
                         case 14:
-                            actual[1, 0].Remove(1);
-                            actual[1, 0].Remove(2);
-                            actual[1, 0].Remove(3);
+                            actual[1, 0].RemoveRange(0, 2);
                             break;
                     }
                 }
-
-                
-
-
-
-                return null;
             }
+        }
 
-            void RemuveRepeatedInRowsAndColumnsSkyscrapers(List<int> list)
+        public static void RemoveRepeatedInRowsAndColumnsSkyscrapers(List<int>[,] list)
+        {
+            for (int sky = 0; sky < 4; sky++)
             {
-                // destroy skyscrapers repeated in a row or column
-                for (int sky = 0; sky < 4; sky++)
+                for (int i = 0; i < 4; i++)
                 {
-                    for (int i = 0; i < 4; i++)
+                    for (int j = 0; j < 4; j++)
                     {
-                        for (int j = 0; j < 4; j++)
+                        if (list[i, j].Count == 1 && list[i, j].Equals(sky))
                         {
-                            if (actual[i, j].Count == 1 && actual[i, j].Equals(sky))
-                            {
-                                for (int r = 0; r < 4; r++)
-                                    if (r != i) actual[i, j].Remove(sky);
-                                for (int c = 0; c < 4; c++)
-                                    if (c != j) actual[i, j].Remove(sky);
-                            }
+                            for (int r = 0; r < 4; r++)
+                                if (r != i) list[i, j].Remove(sky);
+                            for (int c = 0; c < 4; c++)
+                                if (c != j) list[i, j].Remove(sky);
                         }
                     }
                 }
             }
+            return list;
         }
     }
     
