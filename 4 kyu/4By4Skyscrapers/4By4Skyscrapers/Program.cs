f9e218051e41911b214160e6ee501736f742f104
Olga Kripulevich <ochuvasheva@gmail.com>
2019-03-02 20:19:12 +0300

    RemuveRepeatedInRowsAndColumnsSkyscrapers

--- 4 kyu/4By4Skyscrapers/4By4Skyscrapers/Program.cs
+++ 4 kyu/4By4Skyscrapers/4By4Skyscrapers/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _4By4Skyscrapers
 {
@@ -31,61 +28,116 @@ namespace _4By4Skyscrapers
     {
         public static int[][] SolvePuzzle(int[] clues)
         {
-            var actual = new int[3, 3];      
+            var actual = new List<int>[3, 3];
 
-            
+            // in all squares build 1, 2, 3 and 4 skyscraper
+            // then we will destroy unnecessary
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    actual[i, j] = new List<int>() { 1, 2, 3, 4 };
+
+            // build skyscrapers 4
             foreach (int index in clues)
             {
-                if (index == 4)
+                if (index == 1)
                 {
                     switch (index)
                     {
                         case 0:
                         case 15:
-                            actual[0, 0] = 4;
+                            actual[0, 0].Remove(1);
+                            actual[0, 0].Remove(2);
+                            actual[0, 0].Remove(3);
                             break;
                         case 1:
-                            actual[0, 1] = 4;
+                            actual[0, 1].Remove(1);
+                            actual[0, 1].Remove(2);
+                            actual[0, 1].Remove(3);
                             break;
                         case 2:
-                            actual[0, 2] = 4;
+                            actual[0, 2].Remove(1);
+                            actual[0, 2].Remove(2);
+                            actual[0, 2].Remove(3);
                             break;
                         case 3:
                         case 4:
-                            actual[0, 3] = 4;
+                            actual[0, 3].Remove(1);
+                            actual[0, 3].Remove(2);
+                            actual[0, 3].Remove(3);
                             break;
                         case 5:
-                            actual[1, 3] = 4;
+                            actual[1, 3].Remove(1);
+                            actual[1, 3].Remove(2);
+                            actual[1, 3].Remove(3);
                             break;
                         case 6:
-                            actual[2, 3] = 4;
+                            actual[2, 3].Remove(1);
+                            actual[2, 3].Remove(2);
+                            actual[2, 3].Remove(3);
                             break;
                         case 7:
                         case 8:
-                            actual[3, 3] = 4;
+                            actual[3, 3].Remove(1);
+                            actual[3, 3].Remove(2);
+                            actual[3, 3].Remove(3);
                             break;
                         case 9:
-                            actual[3, 2] = 4;
+                            actual[3, 2].Remove(1);
+                            actual[3, 2].Remove(2);
+                            actual[3, 2].Remove(3);
                             break;
                         case 10:
-                            actual[3, 1] = 4;
+                            actual[3, 1].Remove(1);
+                            actual[3, 1].Remove(2);
+                            actual[3, 1].Remove(3);
                             break;
                         case 11:
                         case 12:
-                            actual[3, 0] = 4;
+                            actual[3, 0].Remove(1);
+                            actual[3, 0].Remove(2);
+                            actual[3, 0].Remove(3);
                             break;
                         case 13:
-                            actual[2, 0] = 4;
+                            actual[2, 0].Remove(1);
+                            actual[2, 0].Remove(2);
+                            actual[2, 0].Remove(3);
                             break;
                         case 14:
-                            actual[1, 0] = 4;
+                            actual[1, 0].Remove(1);
+                            actual[1, 0].Remove(2);
+                            actual[1, 0].Remove(3);
                             break;
-
                     }
                 }
 
-            return null;
+                
+
+
+
+                return null;
+            }
+
+            void RemuveRepeatedInRowsAndColumnsSkyscrapers(List<int> list)
+            {
+                // destroy skyscrapers repeated in a row or column
+                for (int sky = 0; sky < 4; sky++)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        for (int j = 0; j < 4; j++)
+                        {
+                            if (actual[i, j].Count == 1 && actual[i, j].Equals(sky))
+                            {
+                                for (int r = 0; r < 4; r++)
+                                    if (r != i) actual[i, j].Remove(sky);
+                                for (int c = 0; c < 4; c++)
+                                    if (c != j) actual[i, j].Remove(sky);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
-
+    
 }
