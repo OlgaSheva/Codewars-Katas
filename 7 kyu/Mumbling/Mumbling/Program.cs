589bc3938a1144d9cb40117218ee8d807d786e92
Olga Kripulevich <ochuvasheva@gmail.com>
2019-03-08 15:23:06 +0300

    Mumbling

---
+++ 7 kyu/Mumbling/Mumbling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Mumbling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Accumul.Accum("ZpglnRxqenU")); // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        }
+    }
+
+    public class Accumul
+    {
+        public static String Accum(string s)
+        {
+            var sArray = s.ToCharArray();
+            var sBuilder = new StringBuilder();
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (i == 0)
+                    sBuilder.Append(Char.ToUpper(sArray[i]));  
+                else
+                {
+                    sBuilder.Append('-');
+                    sBuilder.Append(Char.ToUpper(sArray[i]));
+                    sBuilder.Append(Char.ToLower(sArray[i]), i);
+                }
+            }
+
+            return sBuilder.ToString();
+        }
+    }
+}
