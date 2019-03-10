46b244911e72b98e3655d98bb6e0627d75fbc8ca
Olga Kripulevich <ochuvasheva@gmail.com>
2019-03-10 10:31:26 +0300

    RevealSpyIdentity

---
+++ 7 kyu/RevealSpyIdentity/RevealSpyIdentity/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevealSpyIdentity
+{
+    public abstract class Human
+    {
+        public string Name { get; set; }
+    }
+
+    public class Police : Human { }
+
+    public class Spy : Human { }
+
+    public class Kata
+    {
+        public static string FindHisIdentity(Human person)
+        {
+            if (person is Police)
+                return $"{person.Name} is a Police";
+            else if (person is Spy)
+                return $"{person.Name} is a Spy";
+            else return null;
+        }
+    }
+}
