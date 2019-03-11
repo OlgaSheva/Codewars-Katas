5804ee475b6b3636b573e6a6fec375c059259db9
Olga Kripulevich <ochuvasheva@gmail.com>
2019-03-11 15:42:47 +0300

    PersistentBugger

---
+++ 6 kyu/PersistentBugger/PersistentBugger/Persist.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersistentBugger
+{
+    public class Persist
+    {
+        public static int Persistence(long n)
+        {
+            if (n < 10)
+                return 0;
+
+            var numbers = new List<long>();
+            long num = n;
+            int count = 0;
+
+            do
+            {
+                numbers.Clear();
+
+                while (num > 9)
+                {
+                    numbers.Add(num % 10);
+                    num /= 10;
+                }
+                numbers.Add(num);
+                num = 1;
+
+                foreach (var i in numbers)
+                    num *= i;
+
+                count++;
+            } while (num > 9);
+
+            return count;
+        }
+    }
+}
---
+++ 6 kyu/PersistentBugger/PersistentBuggerTests/PersistTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersistentBugger;
+
+namespace PersistTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Console.WriteLine("****** Basic Tests");
+            Assert.AreEqual(3, Persist.Persistence(39));
+            Assert.AreEqual(0, Persist.Persistence(4));
+            Assert.AreEqual(2, Persist.Persistence(25));
+            Assert.AreEqual(4, Persist.Persistence(999));
+        }
+    }
+}
