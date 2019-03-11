e20198ce5ce53fcb2c2967215d462ff201908892
Olga Kripulevich <ochuvasheva@gmail.com>
2019-03-11 15:43:05 +0300

    AssemblyInfo

---
+++ 7 kyu/OrderedCountOfCharacters/OrderedCountOfCharacters/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderedCountOfCharacters
+{
+    public class Kata
+    {
+        public static List<Tuple<char, int>> OrderedCount(string input)
+        {
+            var charInput = input.ToCharArray();
+            var output = new List<Tuple<char, int>>();
+
+            var mapCharInput = new HashSet<char>();
+            foreach(var c in charInput)
+                mapCharInput.Add(c);
+
+            int count;
+            foreach (var m in mapCharInput)
+            {
+                count = 0;
+                foreach (var c in charInput)
+                {
+                    if (m == c)
+                        count++;
+                }
+                output.Add(new Tuple<char, int>(m, count));
+            }
+
+            return output;
+        }
+    }
+}
